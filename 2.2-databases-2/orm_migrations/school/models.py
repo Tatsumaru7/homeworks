284933a88fbde5102e5a8cf6bd227a2513d75e57
George <>
2023-08-01 16:32:07 +0300

    2_2_1_homework

--- 2.2-databases-2/orm_migrations/school/models.py
+++ 2.2-databases-2/orm_migrations/school/models.py
@@ -1,8 +1,34 @@
+#from django.db import models
+
+
+##class Teacher(models.Model):
+ #   name = models.CharField(max_length=50, verbose_name='Имя')
+ #   subject = models.CharField(max_length=10, verbose_name='Предмет')
+#
+ #   class Meta:
+ #       verbose_name = 'Учитель'
+ #       verbose_name_plural = 'Учителя'
+#
+ #   def __str__(self):
+ #       return self.name
+#
+#
+#class Student(models.Model):
+ #   name = models.CharField(max_length=30, verbose_name='Имя')
+ #   teachers = models.ManyToManyField(Teacher, related_name='students')
+ #   group = models.CharField(max_length=10, verbose_name='Класс')
+#
+ #   class Meta:
+ #       verbose_name = 'Ученик'
+ #       verbose_name_plural = 'Ученики'
+#
+ #   def __str__(self):
+ #       return f'{self.name}'
 from django.db import models
 
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Имя')
+    name = models.CharField(max_length=30, verbose_name='Имя')
     subject = models.CharField(max_length=10, verbose_name='Предмет')
 
     class Meta:
@@ -23,4 +49,4 @@ class Student(models.Model):
         verbose_name_plural = 'Ученики'
 
     def __str__(self):
-        return f'{self.name}'
+        return self.name
