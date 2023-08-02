0aec32ec2aea20df2e5bacf2dc9fe023135bc08b
George <>
2023-08-02 10:08:12 +0300

    upgrade2.2.1

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -7,8 +7,6 @@ from .models import Student, Teacher
 def students_list(request):
     
     object_list = Student.objects.all()
-    for st in object_list:
-        print(st.teachers)
     template = 'school/students_list.html'
     context = {
         'object_list' : object_list
