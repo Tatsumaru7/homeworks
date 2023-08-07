bdc104ed0d7cfcce60e6dfa4efc5aaf7948a6be5
George <>
2023-08-07 15:58:14 +0300

    itog_hw_2.2

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,33 +1,3 @@
-# from django.db import models
-
-
-# class Article(models.Model):
-
-#     title = models.CharField(max_length=256, verbose_name='Название')
-#     text = models.TextField(verbose_name='Текст')
-#     published_at = models.DateTimeField(verbose_name='Дата публикации')
-#     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-#     tags = models.ManyToManyField('Tag', related_name='scopes', through='Scope', blank=True)
-
-#     class Meta:
-#         verbose_name = 'Статья'
-#         verbose_name_plural = 'Статьи'
-#         ordering = ('-published_at',)
-
-#     def __str__(self):
-#         return self.title
-
-# class Tag(models.Model):
-#     name = models.CharField(max_length=256, verbose_name='РАЗДЕЛ', default='default') 
-#     # article = models.ManyToManyField(Article, related_name='article_tag', through='Scope')
-#     def __str__(self):
-#         return self.name
-
-# class Scope(models.Model):
-#     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-#     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-#     is_main = models.BooleanField( verbose_name='ОСНОВНОЙ')
-
 from django.db import models
 
 class Article(models.Model):
