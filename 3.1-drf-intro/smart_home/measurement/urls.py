aa4171f404b3f59cd6ab0e474313d4c05694cb68
George <>
2023-08-09 17:44:14 +0300

    3.1_homework

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-
+from measurement.views import SensorView, MeasureView
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/<pk>/', SensorView.as_view()),
+    path('sensors/', SensorView.as_view()),
+    path('measurements/', MeasureView.as_view()),
 ]
+
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,26 @@
+from django.shortcuts import render
+# from rest_framework.decorators import api_view
+from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from .serializers import MeasurementSerializer, SensorDetailSerializer
+from .models import Sensor, Measurement
+
+# @api_view
+class SensorView(ListCreateAPIView):
+       queryset = Sensor.objects.all()
+       serializer_class = SensorDetailSerializer
+       def get_queryset(self):
+               return Sensor.objects.all()
+       
+       def post(self, request, *args, **kwargs):
+               return super().post(request, *args, **kwargs)
+
+class MeasureView(RetrieveUpdateAPIView):
+        queryset = Measurement.objects.all()
+        serializer_class = MeasurementSerializer
+        def get_queryset(self):
+                return super().get_queryset()
+
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
